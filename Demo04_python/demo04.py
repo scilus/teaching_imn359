60cdccc662aa391dc600bdf68f871f7c3468445e
Charles Poirier <charles.poirier@usherbrooke.ca>
2024-12-06 15:36:00 -0500

    Refactor demo 04

--- Demo04_python/demo04.py
+++ Demo04_python/demo04.py
@@ -7,19 +7,22 @@ import numpy as np
 A = 10
 t = np.arange(-3.0, 3.01, 0.01)
 f1 = np.abs(A * np.sin(np.pi * t)) # petit truc pour rendre sin periodique de periode T
+
+# on initialise f à 0 et on évalue la série pour n = [-10, 10]
 f=0
 for n in range(-10, 11):
     f += (-2 * A / np.pi * (1 / (4 * n * n - 1)) + 0j) * np.exp(1j * 2 * np.pi * n * t)
-    
-plt.plot(t, f, 'b', linewidth=2)
-plt.plot(t, f1, 'r', linewidth=2)
+
+plt.plot(t, f.real, 'b', linewidth=2)
+plt.plot(t, f1, 'r', linewidth=3, linestyle='dotted')
+plt.legend(['SF', 'Fonction originale'])
 plt.savefig('demo04_ex1.jpg')
 plt.show()
 
 # Exemple 2 SF complexe
 # f(t) = Ad/T sum( sinc( w*n*d / 2) * exp(i*w*n*t);
 A = 1
-T = 20 # (2 * d)
+T = 20  # (2 * d)
 d = 10
 w = 2 * np.pi / T
 t = np.arange(-35, 35.1, 0.1)
@@ -32,12 +35,12 @@ for n in range(-10000, 10000):
         f += A * d / T * np.exp(1j * w * n * t)
 
 plt.clf()
-plt.plot(t, f, linewidth=2)
+plt.plot(t, f.real, linewidth=2)
 plt.savefig('demo04_ex2.jpg')
 plt.show()
 
 # Spectre d'amplitude
-# f(w) = |c_n| for n valeur de w
+# f(w) = |c_n| pour n valeur de w
 
 w = np.arange(-1600 * np.pi, 1600 * np.pi, 10)
 A = 1
@@ -51,7 +54,7 @@ plt.plot(w, f, '.-', linewidth=2)
 plt.savefig('demo04_ex2_amp.jpg')
 plt.show()
 
-
+# TODO: What's this next demo about?
 K = 100000
 x = np.arange(-np.pi, np.pi, 0.01)
 f = 0
@@ -60,8 +63,8 @@ for n in range(1, K):
     f += 2 * pow(-1, n+1) / n * np.sin(n * x)
 
 plt.clf()
-plt.plot(x, x, 'r', linewidth=2)
 plt.plot(x, f, 'b')
+plt.plot(x, x, 'r', linestyle='dotted', linewidth=3)
 plt.title('Nombre d harmoniques n = {}'.format(str(K)))
 plt.savefig('demo04_ex2_erreur.jpg')
 plt.show()
@@ -71,10 +74,11 @@ print('Erreur quadratique numerique = {}'.format(err))
 
 # Gibbs ringing
 x = np.arange(-20, 20.1, 0.1)
+ordre_max = 50
 
 f = 3/2
 
-for n in range(1, 51):
+for n in range(1, ordre_max + 1):
     f += 3 * (1 - np.cos(n * np.pi)) / (n * np.pi) * np.sin(n * np.pi * x / 5)
 
 plt.clf()
