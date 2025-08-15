28983a95318586627827aab7c731b0048917a13a
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-08-15 09:00:02 -0400

    Demo 07

--- Demo07_python/demo07.py
+++ Demo07_python/demo07.py
@@ -24,39 +24,42 @@ plt.title(f"TF du signal avec peak à la fréquence {f0}")
 plt.xlabel("Frequence (Hz)")
 plt.show()
 
-
-plt.clf()
+# échantillonnage avec peigne de Dirac
 ts = 20
 ii = np.arange(1, len(t), ts)
 t0 = t[ii]
 d = delta(t, t0)
 
-
-plt.clf()
-plt.plot(t, y, 'b-', t, d, 'ro-', linewidth=2)
-plt.show()
-
 ys = y * d
 t2 = t[ys != 0]
 ys = ys[ys != 0]
 
-plt.clf()
-plt.plot(t2, ys, 'bo-', linewidth=2)
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(t, y, 'b-', t, d, 'ro-', linewidth=2)
+ax[1].plot(t2, ys, 'bo-', linewidth=2)
 plt.show()
 
-
 # Exemple de repliement
-lim = 17
-t1 = np.arange(0, 1, 1/(1000+1)) # ts = 0.001 second, fs = 1000
-t2 = np.arange(0, 1, 1/(100+1)) # ts = 0.01 second, fs = 100
-t3 = np.arange(0, 1, 1/(50+1)) # ts = 0.05 second, fs = 20
-t4 = np.arange(0, 1, 1/(8+1)) # ts = 1/8 second, fs = 8
-t5 = np.arange(0, 1, 1/lim)
+lim = 16
 
+# Différents temps d'échantillonnage
+ts1 = 1/1000
+ts2 = 1/100
+ts3 = 1/50
+ts4 = 1/8
+ts5 = 1/lim
+
+t1 = np.arange(0, 1 + ts1, ts1) # ts = 0.001 second, fs = 1000
+t2 = np.arange(0, 1 + ts2, ts2) # ts = 0.01 second, fs = 100
+t3 = np.arange(0, 1 + ts3, ts3) # ts = 0.05 second, fs = 20
+t4 = np.arange(0, 1 + ts4, ts4) # ts = 1/8 second, fs = 8
+t5 = np.arange(0, 1 + ts5, ts5) # jouez avec lim pour changer t5
+
+# Frequence fondamentale
 f1 = 8
 f2 = 3
 
-# Frequence fondamentale
+# Fonction échantillonnée pour différentes fréquences d'échantillonnage
 s1 = np.cos(2 * np.pi * f1 * t1) + np.cos(2 * np.pi * f2 * t1)
 s2 = np.cos(2 * np.pi * f1 * t2) + np.cos(2 * np.pi * f2 * t2)
 s3 = np.cos(2 * np.pi * f1 * t3) + np.cos(2 * np.pi * f2 * t3)
@@ -71,11 +74,18 @@ axs[3].plot(t4, s4, '-bo')
 axs[4].plot(t5, s5, '-bo')
 plt.show()
 
-f1 = range(-500, 500+1)
-f2 = range(-50, 50+1)
-f3 = range(-25, 25+1)
-f4 = range(-4, 4+1)
-f5 = range(int(-lim/2), int(lim/2)+1)
+# Et dans Fourier...
+fs1 = 1/ts1
+fs2 = 1/ts2
+fs3 = 1/ts3
+fs4 = 1/ts4
+fs5 = 1/ts5
+
+f1 = np.arange(-fs1/2, fs1/2+1)
+f2 = np.arange(-fs2/2, fs2/2+1)
+f3 = np.arange(-fs3/2, fs3/2+1)
+f4 = np.arange(-fs4/2, fs4/2+1)
+f5 = np.arange(-fs5/2, fs5/2+1)
 
 fig, axs = plt.subplots(4,1)
 axs[0].plot(f1, np.real(fftshift(fft(s1))), 'o', linewidth=2)
@@ -89,14 +99,11 @@ plt.plot(f5, np.real(fftshift(fft(s5))), 'o', linewidth=2)
 plt.show()
 plt.clf()
 
-
-
-# Echantillonnage du piece regulare
-M = loadmat("piece-regular.mat")['x0']
+# Echantillonnage du piece regular
+M = np.load('piece-regular.npy')
 
 # C'est comme si on avait echantillonne le signal a tous les 1/1024 pts
 # ou 1024 Hz
-M = loadmat("piece-regular.mat")['x0']
 M = M.squeeze()
 t = np.linspace(0, 1, np.shape(M)[0])
 plt.plot(t, M, '--.', linewidth=2)
@@ -108,10 +115,8 @@ f = np.linspace(-512, 512, 1024)
 plt.plot(range(-512, 512), fftshift(np.abs(fft(M))), '--.r', linewidth=2)
 plt.show()
 
-
 # On voit que la fft est symetrique. En fait, on a que de l'information sur
 # 1024/2 frequences a cause des coefficients complexe de la TF
 plt.clf()
 plt.plot(f, np.abs(fftshift(fft(M))))
 plt.show()
-
