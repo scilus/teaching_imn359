54710d8fcee9c481e64650134cd52616a0adef96
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-08-19 09:17:59 -0400

    Demo09 premiere passe

--- Demo09_python/Demo09.py
+++ Demo09_python/Demo09.py
@@ -4,10 +4,8 @@ from scipy.io import loadmat
 
 from dct import dct
 from idct import idct
-from dct2 import dct2
-from idct2 import idct2
 
-from scipy.fft import fft, fftshift, ifftshift, ifft
+from scipy.fft import fft, fftshift, ifft
 
 # DCT locale
 ######################################################
@@ -16,16 +14,16 @@ from scipy.fft import fft, fftshift, ifftshift, ifft
 # Ameliore l'approximation globale de la DCT
 # On decoupe le signal en segments locaux  dans lesquels on
 # fait la DCT
-# JPEG est basée sur cette base
+# Exemple avec un signal sonore
 # ####################################################
 
-x = loadmat('bird.mat')['x']
+x = np.load('bird.npy')
 n = x.shape[0]
 fc = dct(x)
 
+# On peut écouter l'enregistrement audio
 import sounddevice as sd
 sd.play(x, n)
-
 
 w = 16 # taille du segment
 
@@ -38,22 +36,27 @@ for i in range(int(n/w)):
 
 fig, axs = plt.subplots(2, 1)
 axs[0].plot(np.abs(fc))
+axs[0].set_title('DCT')
 axs[1].plot(np.abs(fc_a))
+axs[1].set_title('DCT locale')
+fig.tight_layout()
 plt.show()
 
-# Lets keep 4 lowest frequency coefficients in each bin (4* n/w in total)
-# iDCT locale
+# Reconstruction du signal avec full DCT
 f1 = idct(fc)
-fm_local = fc_a
+
+# iDCT locale
+fm_local = np.zeros((n, 1))
 fc_a_4 = np.zeros((n, 1))
 fm_a = np.zeros((n, 1))
 
+# On garde les 4 plus basses fréquences de chaque segment (4*n/w au total)
 s = 4
 for i in range(int(n/w)):
     seli = np.array([i * w, i * w + w])
     sela = np.array([i * w, i * w + s])
-    fc_a_4[sela[0]:sela[1]] = fm_local[sela[0]:sela[1]]
-    fm_local[seli[0]:seli[1]] = idct(fm_local[seli[0]:seli[1]])
+    fc_a_4[sela[0]:sela[1]] = fc_a[sela[0]:sela[1]]
+    fm_local[seli[0]:seli[1]] = idct(fc_a[seli[0]:seli[1]]) # on fait la iDCT locale non-tronquée aussi
     fm_a[seli[0]:seli[1]] = idct(fc_a_4[seli[0]:seli[1]])
 
 f = x
@@ -68,7 +71,7 @@ ffamp = np.abs(fftshift(ff))
 ffs = fftshift(ff)
 
 ffs_2 =  np.zeros((n, 1))
-ffs_2[int(n/2 - coeff):int(n/2 + coeff)] = ffs[int(n/2 - coeff):int(n/2 + coeff)] 
+ffs_2[int(n/2 - coeff):int(n/2 + coeff)] = ffs[int(n/2 - coeff):int(n/2 + coeff)]
 
 xx = np.abs(ifft(fftshift(ffs_2), axis=0))
 
@@ -89,4 +92,5 @@ axs[3].plot(f1_2)
 axs[3].set_title('256 coeffs of DCT, error = ' + str(np.linalg.norm(f-f1_2)/np.linalg.norm(f)))
 axs[4].plot(xx)
 axs[4].set_title('256 coeffs of FFT, error = ' + str(np.linalg.norm(f-xx)/np.linalg.norm(f)))
+fig.tight_layout()
 plt.show()
